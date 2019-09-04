dbd1f46cbccec2c9ce03980890569770d22573e5
Lutaaya Brian Ivan <briantical@briantical.local>
2019-09-04 20:09:33 +0300

    installing firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID
+};
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/storage");
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
