ec7404e2217ecf4eb5501b5c803053483dccccd9
Lutaaya Brian Ivan <briantical@briantical.local>
2019-09-18 19:48:18 +0300

    working with axios

--- src/containers/pages/Login/Login.js
+++ src/containers/pages/Login/Login.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const Login = () => (
-    <div>
-        <p>THIS IS THE LOGIN PAGE</p>
-        <Link to="/register" >REGISTER</Link>
-    </div>
-)
+export class Login extends Component {
+    componentDidMount() {
+        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkN2VhMGE0MTkyNGYxMGI5MTg3NDczZiIsImlhdCI6MTU2ODc1MzkwOSwiZXhwIjoxNTcxMzQ1OTA5fQ.xh1xXYb4OLXUMYvKYRsugxektjOgKpwCx2O42qY7o_Q";
+        let config = {
+            headers: {'Authorization': "bearer " + token}
+        };
+        
+        // let bodyParameters = {
+        //    key: "value"
+        // }
+        
+        axios.post( 
+          'http://localhost:3000/api/v1/cake/?skip=0&limit=1&search=cakeDetail',
+        //  bodyParameters,
+          config
+        ).then((response) => {
+          console.log(response)
+        }).catch((error) => {
+          console.log(error)
+        });
+    }
 
-export default Login;
+    render() {
+        return (
+            <div>
+                <p>THIS IS THE LOGIN PAGE</p>
+                <Link to="/register" >REGISTER</Link>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    
+})
+
+const mapDispatchToProps = {
+    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
