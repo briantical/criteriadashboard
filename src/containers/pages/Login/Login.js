17cd27c7d715ea25c5c431e80fa7d66029d60e8a
Lutaaya Brian Ivan <briantical@briantical.local>
2019-09-22 02:15:51 +0300

    Working on login and registr

--- src/containers/pages/Login/Login.js
+++ src/containers/pages/Login/Login.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
-import { Spinner } from 'react-bootstrap';
+import { withRouter , Link } from 'react-router-dom';
 
 import './Login.css';
 import { setActiveUserToken }  from '../../../actions'
 
 export class Login extends Component {
-  componentDidMount() {
-      //this.signin();
+  constructor() {
+      super()
+      this.state ={
+        displayErrors : false
+      }
   }
 
    signin = (email,password) =>{
@@ -22,18 +25,60 @@ export class Login extends Component {
       params
     ).then((response) => {
       this.props.setActiveUserToken(response.data.token);
+      this.props.history.push('/');
     }).catch((error) => {
       console.log(error)
     });
   }
 
+  handleSubmit = (event) =>{
+    event.preventDefault();
+    if(!event.target.checkValidity()){
+      // form is invalid! so we do nothing
+      this.setState({displayErrors : true})
+      return;
+    }
+    const form = event.target;
+    const data = new FormData(form);
+
+    let email = data.get('email');
+    let password = data.get('password');
+
+    this.signin(email , password);
+  }
+
   render() {
+    const { displayErrors } = this.state;
     return (
-      <div className='login container-fluid'>
-        <Spinner animation="grow" role="status" size="sm" style={{color:'#4F0B0C'}}>
-          <span className="sr-only">Loading...</span>
-        </Spinner>
-        {this.props.activeUser}
+      <div className='login'>
+        <form 
+          onSubmit={this.handleSubmit} 
+          noValidate 
+          className={displayErrors ? 'displayErrors' : ''}>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+              Email :
+              </td>
+              <td>
+                <input type="email" placeholder="jondoe@gmail.com" name="email" id="email" autoComplete="off" required/>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Password :
+              </td>
+              <td>
+                <input type="password" name="password" id="password" required/>
+              </td>
+            </tr>
+            </tbody>
+        </table>
+        <button>LOGIN</button>
+        </form>
+        
+        <Link to="/register" >SIGN UP</Link>
       </div>
     )
   }
@@ -48,4 +93,4 @@ const mapDispatchToProps = {
   setActiveUserToken    
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Login));
--- src/containers/pages/Register/Register.js
+++ src/containers/pages/Register/Register.js
@@ -1,12 +1,92 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import { withRouter , Link } from 'react-router-dom';
 
-const Register = () => (
-    <div>
-        <p>THIS IS THE REGISTER PAGE</p>
-        <Link to="/login" >LOGIN</Link>
-        <Link to="/" >DASHBOARD</Link>
-    </div>
-)
+export class Register extends Component {
+    constructor() {
+        super()
+        this.state ={
+          displayErrors : false
+        }
+    }
+  
+     signup = (email,password) =>{
+      let params ={
+        email,
+        password
+      }
+      
+      axios.post( 
+        'http://localhost:3000/api/v1/auth/sign-up',
+        params
+      ).then((response) => {
+        this.props.history.push('/login');
+      }).catch((error) => {
+        console.log('Error' + error)
+      });
+    }
 
-export default Register;
+    handleSubmit = (event) =>{
+        event.preventDefault();
+        if(!event.target.checkValidity()){
+          // form is invalid! so we do nothing
+          this.setState({displayErrors : true})
+          return;
+        }
+        const form = event.target;
+        const data = new FormData(form);
+    
+        let email = data.get('email');
+        let password = data.get('password');
+    
+        this.signup(email , password);
+      }
+
+    render() {
+        const { displayErrors } = this.state;
+        return (
+        <div className='login'>
+            <form 
+            onSubmit={this.handleSubmit} 
+            noValidate 
+            className={displayErrors ? 'displayErrors' : ''}>
+            <table>
+            <tbody>
+                <tr>
+                <td>
+                Email :
+                </td>
+                <td>
+                    <input type="email" placeholder="jondoe@gmail.com" name="email" id="email" autoComplete="off" required/>
+                </td>
+                </tr>
+                <tr>
+                <td>
+                    Password :
+                </td>
+                <td>
+                    <input type="password" name="password" id="password" required/>
+                </td>
+                </tr>
+                </tbody>
+            </table>
+            <button>SIGN UP</button>
+            </form>
+            
+            <Link to="/login" >Login instead</Link>
+        </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    const { activeUser } = state;
+    return { activeUser };
+};
+
+const mapDispatchToProps = {
+    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Register))
