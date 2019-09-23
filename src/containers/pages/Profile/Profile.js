4268ef0fb5b7971f08b292ab4b21489eea7b70e8
Lutaaya Brian Ivan <briantical@briantical.local>
2019-09-24 00:36:30 +0300

    Added profile route details

--- src/containers/pages/Profile/Profile.js
+++ src/containers/pages/Profile/Profile.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 const Profile = () => (
     <div>
-        <p>THIS IS THE PROFILE PAGE</p>
+        <p>COMPLETE PROFILE PAGE</p>
+        <form>
+            <table>
+                <tbody>
+                    <tr>
+                        <td><img src={require ("../../../assets/MOI.jpg")} className="rounded" alt="profile picture"/></td>
+                        <td>NAME: YOUR EXCITING</td>
+                    </tr>
+                    <tr>
+                        <td>ADDRESS</td>
+                        <td><input name="address" id="address" autoComplete="off"/></td>
+                    </tr>
+                    <tr>
+                        <td>EMAIL</td>
+                        <td><input name="email" id="email" autoComplete="off"/></td>
+                    </tr>
+                    <tr>
+                        <td>PHONE NUMBER</td>
+                        <td><input name="phone" id="spank" autoComplete="off"/></td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+        <button>COMPLETE</button>
     </div>
 )
 
