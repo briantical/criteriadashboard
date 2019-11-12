01404905f49462ad0d784eaf49f3a78f4c3a25be
Lutaaya Brian Ivan <briantical@briantical.local>
2019-11-12 20:02:19 +0300

    adding prettier

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { BrowserRouter as Router , Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import PropTypes from "prop-types";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import Index from './containers/Index'
+import Index from "./containers/Index";
 
-const  App = ({store}) => (
+const App = ({ store }) => (
   <Provider store={store}>
     <Router>
-      <Route path = "/:filter?" component={ Index } />
+      <Route path="/:filter?" component={Index} />
     </Router>
   </Provider>
-)
+);
 
 App.propTypes = {
-  store : PropTypes.object.isRequired
-}
+  store: PropTypes.object.isRequired
+};
 
 export default App;
