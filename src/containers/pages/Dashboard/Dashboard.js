3ae69fdc802e4b29160502e7d00a6f1900041c05
Lutaaya Brian Ivan <briantical@briantical.local>
2019-11-14 20:15:48 +0300

    Securing access to dashboard path

--- src/containers/pages/Dashboard/Dashboard.js
+++ src/containers/pages/Dashboard/Dashboard.js
@@ -1,113 +1,165 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-import './Dashboard.css';
+import "./Dashboard.css";
 
-import { 
-    setModalVisibility,
-    setActiveUser,
-    setUserEmail
-} from '../../../actions';
+import {
+  setModalVisibility,
+  setActiveUser,
+  setUserEmail
+} from "../../../actions";
 
-import { 
-    Sidebar , 
-    Main , 
-    Profilemodal, 
-    Editcakemodal, 
-    Addcakemodal, 
-    Addsnackmodal, 
-    Editsnackmodal,
-    Addaddonmodal, 
-    Editaddonmodal
-} from '../components';
-import { 
-    profilemodal, 
-    editcakemodal, 
-    addcakemodal,
-    addsnackmodal,
-    editsnackmodal,
-    addaddonmodal,
-    editaddonmodal
-} from '../../../constants/modals';
-import { secureStorage } from '../../../utils'
-
+import {
+  Sidebar,
+  Main,
+  Profilemodal,
+  Editcakemodal,
+  Addcakemodal,
+  Addsnackmodal,
+  Editsnackmodal,
+  Addaddonmodal,
+  Editaddonmodal
+} from "../components";
+import {
+  profilemodal,
+  editcakemodal,
+  addcakemodal,
+  addsnackmodal,
+  editsnackmodal,
+  addaddonmodal,
+  editaddonmodal
+} from "../../../constants/modals";
+import { secureStorage } from "../../../utils";
 
 export class Dashboard extends Component {
+  componentDidMount = () => {
+    let token = secureStorage.getItem("token");
+    console.log("The token 1:" + token);
+    let { user, setActiveUser, history } = this.props;
 
-    componentDidMount = ()=>{
-        let { user, setActiveUser, history } = this.props;
-        user = user == null ? secureStorage.getItem('user').user : user;
+    user =
+      secureStorage.getItem("user") !== null
+        ? secureStorage.getItem("user").user
+        : user;
+    console.log(user);
 
-        let setupuser = () => {
-            setActiveUser(user);
-            setUserEmail(secureStorage.getItem('email').email);
-        }
+    let setupuser = () => {
+      setActiveUser(user);
+      setUserEmail(secureStorage.getItem("email").email);
+    };
 
-        if (user !== null) {
-            const { profile:{complete} } = user;
-            complete ? 
-            setupuser()
-            :
-            history.push('/login');
-        }else{
-            history.push('/login');
-        } 
+    if (user !== null) {
+      const {
+        profile: { complete }
+      } = user;
+      complete ? setupuser() : history.push("/login");
+    } else {
+      history.push("/login");
     }
+  };
 
-    closeModal =(modal)=>{
-        this.props.setModalVisibility(false,modal,null);
-    }
-    
-    chooseModal = (modal, modalprops) =>{
-        switch (modal) {
-            case profilemodal:
-                return <Profilemodal hideModal={()=>this.closeModal(profilemodal)} modalprops={modalprops}/>
+  closeModal = modal => {
+    this.props.setModalVisibility(false, modal, null);
+  };
 
-            case editcakemodal:
-                return <Editcakemodal hideModal={()=>this.closeModal(editcakemodal)} modalprops={modalprops}/>
-
-            case addcakemodal:
-                return <Addcakemodal hideModal={()=>this.closeModal(editcakemodal)} modalprops={modalprops}/>
-
-            case editsnackmodal:
-                return <Editsnackmodal hideModal={()=>this.closeModal(editsnackmodal)} modalprops={modalprops}/>
-    
-            case addsnackmodal:
-                return <Addsnackmodal hideModal={()=>this.closeModal(editsnackmodal)} modalprops={modalprops}/>
-
-            case editaddonmodal:
-                return <Editaddonmodal hideModal={()=>this.closeModal(editaddonmodal)} modalprops={modalprops}/>
-    
-            case addaddonmodal:
-                return <Addaddonmodal hideModal={()=>this.closeModal(editaddonmodal)} modalprops={modalprops}/>
-        
-            default:
-                break;
-        }
-    }
-
-    render() {
-        const {showModal:{show, modal,modalprops} } = this.props;
+  chooseModal = (modal, modalprops) => {
+    switch (modal) {
+      case profilemodal:
         return (
-            <div className='dashboard'>
-                <Sidebar/>
-                <Main/>
-                { show ? this.chooseModal(modal, modalprops ) : null}
-            </div>
-        )
+          <Profilemodal
+            hideModal={() => this.closeModal(profilemodal)}
+            modalprops={modalprops}
+          />
+        );
+
+      case editcakemodal:
+        return (
+          <Editcakemodal
+            hideModal={() => this.closeModal(editcakemodal)}
+            modalprops={modalprops}
+          />
+        );
+
+      case addcakemodal:
+        return (
+          <Addcakemodal
+            hideModal={() => this.closeModal(editcakemodal)}
+            modalprops={modalprops}
+          />
+        );
+
+      case editsnackmodal:
+        return (
+          <Editsnackmodal
+            hideModal={() => this.closeModal(editsnackmodal)}
+            modalprops={modalprops}
+          />
+        );
+
+      case addsnackmodal:
+        return (
+          <Addsnackmodal
+            hideModal={() => this.closeModal(editsnackmodal)}
+            modalprops={modalprops}
+          />
+        );
+
+      case editaddonmodal:
+        return (
+          <Editaddonmodal
+            hideModal={() => this.closeModal(editaddonmodal)}
+            modalprops={modalprops}
+          />
+        );
+
+      case addaddonmodal:
+        return (
+          <Addaddonmodal
+            hideModal={() => this.closeModal(editaddonmodal)}
+            modalprops={modalprops}
+          />
+        );
+
+      default:
+        break;
     }
+  };
+
+  render = () => {
+    let token = secureStorage.getItem("token");
+    console.log("The token 2:" + token);
+    const {
+      showModal: { show, modal, modalprops },
+      history
+    } = this.props;
+    if (token !== null) {
+      return (
+        <div className="dashboard">
+          <Sidebar />
+          <Main />
+          {show ? this.chooseModal(modal, modalprops) : null}
+        </div>
+      );
+    } else {
+      history.push("/login");
+      return null;
+    }
+  };
 }
 
-const mapStateToProps = (state) => {
-    const { user, showModal } = state;
-    return { user , showModal};  
+const mapStateToProps = state => {
+  const { user, showModal } = state;
+  return { user, showModal };
 };
 
 const mapDispatchToProps = {
-    setModalVisibility,
-    setActiveUser,
-    setUserEmail
-}
+  setModalVisibility,
+  setActiveUser,
+  setUserEmail
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Dashboard));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(Dashboard));
--- src/containers/pages/Login/Login.js
+++ src/containers/pages/Login/Login.js
@@ -1,72 +1,91 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import axios from 'axios';
-import { withRouter , Link } from 'react-router-dom';
-import SpringSpinner from '@bit/bondz.react-epic-spinners.spring-spinner';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+import { withRouter, Link } from "react-router-dom";
+import SpringSpinner from "@bit/bondz.react-epic-spinners.spring-spinner";
 
-import './Login.css';
-import { setActiveUser, setErrorMessage, setUserToken, setUserEmail, showLoadingSpinner }  from '../../../actions';
-import { secureStorage } from '../../../utils';
+import "./Login.css";
+import {
+  setActiveUser,
+  setErrorMessage,
+  setUserToken,
+  setUserEmail,
+  showLoadingSpinner
+} from "../../../actions";
+import { secureStorage } from "../../../utils";
 
 export class Login extends Component {
-    componentDidMount(){
-      let errorMessage = {message: "", show: false};
-      this.props.setErrorMessage(errorMessage);
-    }
-    
-   signin = (email,password) =>{
-    let params ={
+  componentDidMount() {
+    let errorMessage = { message: "", show: false };
+    this.props.setErrorMessage(errorMessage);
+  }
+
+  signin = (email, password) => {
+    let params = {
       email,
       password
     };
     let options = {
       responseType: "json"
     };
-    
-    axios.post( 
-      `${process.env.REACT_APP_URL}/api/v1/auth/sign-in`,
-      params,
-      options
-    )
-    .then((response) => {
-      console.log(response)
-      const {data:{message}} = response;
 
-      if(message !== 'User is not verified'){
-        const { token, user:{profile:{complete}},user} = response.data;
-        const { setActiveUser, setUserToken, setUserEmail, setErrorMessage, history,showLoadingSpinner } = this.props;
+    axios
+      .post(`${process.env.REACT_APP_URL}/api/v1/auth/sign-in`, params, options)
+      .then(response => {
+        console.log(response);
+        const {
+          data: { message }
+        } = response;
 
-        setActiveUser(user);
-        setUserToken(token);
-        secureStorage.setItem('token', {token});
-        secureStorage.setItem('user', {user});
-        secureStorage.setItem('email', {email});
-        setUserEmail(email);
+        if (message !== "User is not verified") {
+          const {
+            token,
+            user: {
+              profile: { complete }
+            },
+            user
+          } = response.data;
+          const {
+            setActiveUser,
+            setUserToken,
+            setUserEmail,
+            setErrorMessage,
+            history,
+            showLoadingSpinner
+          } = this.props;
 
-        showLoadingSpinner(false);
+          setActiveUser(user);
+          setUserToken(token);
+          secureStorage.setItem("token", { token });
+          secureStorage.setItem("user", { user });
+          secureStorage.setItem("email", { email });
+          setUserEmail(email);
 
-        let errorMessage = {message: "", show: false};
-        setErrorMessage(errorMessage);
-        complete ? history.push('/dashboard') : history.push({pathname:'/profile',search:email});
-      }else{
-        let errorMessage = {message: message, show: true};
-        this.props.setErrorMessage(errorMessage);
+          showLoadingSpinner(false);
+
+          let errorMessage = { message: "", show: false };
+          setErrorMessage(errorMessage);
+          complete
+            ? history.push("/dashboard")
+            : history.push({ pathname: "/profile", search: email });
+        } else {
+          let errorMessage = { message: message, show: true };
+          this.props.setErrorMessage(errorMessage);
+          this.props.showLoadingSpinner(false);
+        }
+      })
+      .catch(error => {
+        let theError = { message: error.message, show: true };
+        this.props.setErrorMessage(theError);
+        this.props.showLoadingSpinner(true);
         this.props.showLoadingSpinner(false);
-      }
-      
-    })
-    .catch((error) => {
-      let theError = {message:error.message ,show:true}
-      this.props.setErrorMessage(theError);
-      this.props.showLoadingSpinner(true);
-      this.props.showLoadingSpinner(false);
-    });
-  }
+      });
+  };
 
-  handleSubmit = (event) =>{
+  handleSubmit = event => {
     event.preventDefault();
-    if(!event.target.checkValidity()){
-      this.props.setErrorMessage({message:'',show:true});
+    if (!event.target.checkValidity()) {
+      this.props.setErrorMessage({ message: "", show: true });
       return;
     }
     this.props.showLoadingSpinner(true);
@@ -74,62 +93,73 @@ export class Login extends Component {
     const form = event.target;
     const data = new FormData(form);
 
-    let email = data.get('email');
-    let password = data.get('password');
+    let email = data.get("email");
+    let password = data.get("password");
 
-    this.signin(email , password);
-  }
+    this.signin(email, password);
+  };
 
   render() {
-    
     const { errorMessage, spinner } = this.props;
     return (
-      <div className='login'>
-        <form 
-          onSubmit={this.handleSubmit} 
-          noValidate 
-          className={errorMessage.show ? 'displayErrors' : ''}>
-        <table>
-          <tbody>
-            <tr>
-              <td>
-              Email :
-              </td>
-              <td>
-                <input type="email" placeholder="jondoe@gmail.com" name="email" id="email" autoComplete="off" required/>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Password :
-              </td>
-              <td>
-                <input type="password" name="password" id="password" required/>
-              </td>
-            </tr>
+      <div className="login">
+        <form
+          onSubmit={this.handleSubmit}
+          noValidate
+          className={errorMessage.show ? "displayErrors" : ""}
+        >
+          <table>
+            <tbody>
+              <tr>
+                <td>Email :</td>
+                <td>
+                  <input
+                    type="email"
+                    placeholder="jondoe@gmail.com"
+                    name="email"
+                    id="email"
+                    autoComplete="off"
+                    required
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Password :</td>
+                <td>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    required
+                  />
+                </td>
+              </tr>
             </tbody>
-        </table>
-        {spinner ? <SpringSpinner color='#000000' size={parseInt('20')}/> : <button>LOGIN</button>}
+          </table>
+          {spinner ? (
+            <SpringSpinner color="#000000" size={parseInt("20")} />
+          ) : (
+            <button>LOGIN</button>
+          )}
         </form>
         {this.props.errorMessage.show ? this.props.errorMessage.message : null}
-        <Link to="/register" >SIGN UP</Link>
-        
+        <Link to="/register">SIGN UP</Link>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  const { errorMessage, user ,spinner } = state;
+const mapStateToProps = state => {
+  const { errorMessage, user, spinner } = state;
   return { errorMessage, user, spinner };
-}
+};
 
 const mapDispatchToProps = {
   setActiveUser,
   setUserToken,
   setErrorMessage,
   setUserEmail,
-  showLoadingSpinner 
-}
+  showLoadingSpinner
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Login));
