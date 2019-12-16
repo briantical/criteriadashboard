8ef3afec66f2f6664e4688d48b20508b36497eb2
Lutaaya Brian Ivan <briantical@briantical.local>
2019-12-16 21:30:33 +0300

    Tabs managed via redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import store from './store'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import store from "./store";
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
-console.log(store.getState())
+ReactDOM.render(<App store={store} />, document.getElementById("root"));
 serviceWorker.register();
